6d0842c8f6a119cd04203196a29dadb321f9dc32
loyalflower0908 <sceww187@naver.com>
2024-08-04 22:11:32 +0900

    플러터 연습3

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:webtooninfo/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebtoonInfo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      scrollBehavior: const MaterialScrollBehavior().copyWith(dragDevices: {
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.touch,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.unknown
+      }),
+      home: HomeScreen(),
+    );
+  }
+}
